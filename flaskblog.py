286a33edee5b97dae4097822b8b1f7109c59a325
mwangemi <mwangemik@gmail.com>
2021-06-04 17:41:27 +0300

    add bootstrap and requirements.txt

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+   {
+      "author": "Corey Schafer", 
+      "title": "First Post", 
+      "content": "This is a post from a different user 1", 
+      "date_posted": "April 20, 2018"
+   }, 
+   {
+      "author": "Joyce Mayer", 
+      "title": "Second Post", 
+      "content": "This is a post from a different user 2", 
+      "date_posted": "August 20, 2020"
+   } 
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-   return "<h1>Home Page</h1>"
+   return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
-   return "<h1>About Page</h1>"
+   return render_template('about.html', title='About')
 
 if __name__ == '__main__':
    app.run(debug=True)
